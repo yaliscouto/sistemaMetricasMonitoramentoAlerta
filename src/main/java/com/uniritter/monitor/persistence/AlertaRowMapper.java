0a9979fbf72278a32ecb9eada33993e28551884e
ALINE BERNARDI DA ROCHA <ALU201530615@uniritter.local>
2016-06-03 21:05:46 -0300

    Alteração classe AlertaRowMapper

--- src/main/java/com/uniritter/monitor/persistence/AlertaRowMapper.java
+++ src/main/java/com/uniritter/monitor/persistence/AlertaRowMapper.java
@@ -3,16 +3,16 @@ package com.uniritter.monitor.persistence;
 import java.sql.ResultSet;
 
 
+
+import java.sql.SQLException;
+
 import org.springframework.jdbc.core.RowMapper;
 
+import com.uniritter.monitor.domain.Alerta;
 
-
-
-public class AlertaRowMapper {
-	
-      
-
-		@Override
+public class AlertaRowMapper implements RowMapper<Alerta>  {
+	@Override
+	 
 	public Alerta mapRow(ResultSet rs, int rowNum) throws SQLException {
 		return new Alerta(
 				rs.getLong("id"), 
