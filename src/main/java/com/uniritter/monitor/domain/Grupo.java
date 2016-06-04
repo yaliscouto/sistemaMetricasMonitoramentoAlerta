5632a674ca490949c6bcdf21040d2e921196989d
ALINE BERNARDI DA ROCHA <ALU201530615@uniritter.local>
2016-06-03 22:04:25 -0300

    Criação das Classes: Host, Medição, Grupos

---
+++ src/main/java/com/uniritter/monitor/domain/Grupo.java
@@ -0,0 +1,42 @@
+package com.uniritter.monitor.domain;
+
+import java.util.List;
+
+public class Grupo {
+
+	private int id;
+	private int descricao;
+	private List<Host> host;
+
+	public Grupo(int id, int descricao, List<Host> host) {
+		super();
+		this.id = id;
+		this.descricao = descricao;
+		this.host = host;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(int descricao) {
+		this.descricao = descricao;
+	}
+
+	public List<Host> getHost() {
+		return host;
+	}
+
+	public void setHost(List<Host> host) {
+		this.host = host;
+	}
+
+}
--- src/main/java/com/uniritter/monitor/persistence/AlertaRowMapper.java
+++ src/main/java/com/uniritter/monitor/persistence/AlertaRowMapper.java
@@ -5,19 +5,24 @@ import java.sql.ResultSet;
 
 
 import java.sql.SQLException;
+import java.util.List;
 
 import org.springframework.jdbc.core.RowMapper;
 
 import com.uniritter.monitor.domain.Alerta;
+import com.uniritter.monitor.domain.Regra;
+import com.uniritter.monitor.domain.Tipo;
 
 public class AlertaRowMapper implements RowMapper<Alerta>  {
 	@Override
-	 
+	
 	public Alerta mapRow(ResultSet rs, int rowNum) throws SQLException {
+		Tipo tipo;
+		Regra regra ;
 		return new Alerta(
 				rs.getLong("id"), 
 				rs.getString("nome"),
-				rs.getObject("tipo", Tipo),
-				rs.getObject("regras",List<Regra> regras));
+				(List<Regra>) (regra = (Regra)rs.getObject("regras")),
+				tipo = (Tipo) rs.getObject("tipo"));
 	}
 }
